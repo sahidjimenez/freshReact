a397e2adfca2ec438f99466a2d31321196d10b80
sahid <sahidjimenez@wolfsellsers.com>
2022-11-22 12:17:45 -0600

    primer commit

---
+++ src/components/Home/home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ContainerHome from '../../miniComponents/ContainerHome'
+
+const valores = [
+    { name: "haz tu pedido", img: "", route: "/haz-tu-pedido" },
+    { name: "menu", img: "", route: "/menu" },
+    { name: "nosotros", img: "", route: "/nosotros" },
+    { name: "contacto", img: "", route: "/contacto" },
+    { name: "extra", img: "", route: "/extra" }
+]
+
+const Home = () => {
+
+    return (
+        <>
+
+            {valores.map(item => {
+                return <ContainerHome isNavigation={true} name={item.name} />
+                
+            })}
+
+
+        </>
+    )
+
+}
+
+export default Home
---
+++ src/miniComponents/ContainerHome/containerHome.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ContainerHome = (props) => {
+
+    const { isNavigation,name, isCarousel,img,route } = props
+
+    const contenedorNavigation = isNavigation ? (
+        <>
+            <div className='flex justify-center h-40'>
+                <button>
+                    {name?name:null}
+                </button>
+            </div>
+
+        </>
+    ) : null
+    return (
+        <div>
+            {contenedorNavigation}
+
+        </div>
+    )
+
+}
+
+export default ContainerHome
