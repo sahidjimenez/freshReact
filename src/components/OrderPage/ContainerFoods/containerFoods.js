d8911c1eda339200078322a308570e00daec2ee9
sahid <sahidjimenez@wolfsellsers.com>
2023-07-05 10:45:00 -0600

    Revert "forms"
    This reverts commit 281969179e36950cc5cf4b9c07d834220cdb70a5.

--- src/components/OrderPage/ContainerFoods/containerFoods.js
+++ src/components/OrderPage/ContainerFoods/containerFoods.js
@@ -1,60 +1,25 @@
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext } from 'react';
 import Food from './Food/food';
 import css from './containerFoods.module.css';
 import { DataContext } from '../../../context/DataContext';
-import Modal from './Modal/modal';
-import { useEffect } from 'react';
 
 const ContainerFoods = () => {
 
-    const { data, setData } = useContext(DataContext);
+    const { data } = useContext(DataContext);
     const { menu } = data;
-    const [open, setOpen] = useState(false)
-    const [selectedFood, setSelectedFood] = useState({})
-
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const sahid = (item) => {
-        handleClickOpen()
-        setData({
-            ...data,
-            total: "10"
-        })
-    }
-
     return (
         <div className={css.container}>
             <div className={css.containerFoods}>
-                {menu.map((element, i) => {
+                {menu.map((element) => {
 
                     return (
-                        <div key={i}>
-
-                            <div >
-                                <Food
-                                    props={element}
-                                    handleClickOpen={handleClickOpen}
-                                    setSelectedFood={setSelectedFood}
-                                    selectedFood={selectedFood}
-                                    open={open}
-                                    handleClose={handleClose}
-                             
-
-                                />
-                            </div>
-
-                        </div>
+                        <>
+                            <Food props={element} />
+                        </>
                     )
+                    
                 })}
-
+                <Food />
             </div>
         </div>
 
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -5,166 +5,22 @@ export const DataContext = createContext();
 const dataFixed = {
     nombre: 'sahid',
     total: 0,
-    orden: {},
-    menu: [
-        {
-            nombre: 'Ensaldas',
-            comidas: [
-                {
-                    nombre: 'ensalada',
-                    precio: 100,
-                    descripcion: 'Ensalada de lechuga fresca con 18 ingredientes a elegir un aderezo y un complemento.',
-                    urlImage: 'static/ensaladagrande.jpg',
-                    typeInput: 'normal',
-                    input: {
-                        tipo: 'multi',
-                        opciones: [
-
-                            {
-                                nombreOpcion: 'tamaño',
-                                opciones: [
-                                    'chico',
-                                    'mediano',
-                                    'grande'
-                                ]
-                            }
-                            ,
-
-                            {
-                                nombreOpcion: 'proteina',
-                                opciones: [
-                                    'pollo natural',
-                                    'pollo bbq',
-                                    'jamón',
-                                    'pollo frito',
-                                    'pollo especies',
-                                    'atún'
-                                ]
-                            }
-                            ,
-
-                            {
-                                nombreOpcion: 'ingredientes',
-                                opciones: [
-                                    'zanahoria',
-                                    'tomate',
-                                    'betabel',
-                                    'champiñón',
-                                    'jalapeño',
-                                    'cebolla',
-                                    'aceituna',
-                                    'piña',
-                                    'morrón',
-                                    'panela',
-                                    'pasta',
-                                    'apio',
-                                    'col morado',
-                                    'pepino',
-                                    'espinaca',
-                                    'germinado',
-                                    'elote',
-                                    'brócoli',
-                                    'jamón',
-                                    'chorizo',
-                                    'queso manchego'
-                                ]
-                            }
-                            ,
-
-                            {
-                                nombreOpcion: 'aderezo',
-                                opciones: [
-                                    'ranch',
-                                    'mil islas',
-                                    'cesar',
-                                    'italiano',
-                                    'mostaza miel',
-                                    'chimichurry'
-                                ]
-                            }
-                            ,
-
-                            {
-                                nombreOpcion: 'complemento',
-                                opciones: [
-                                    'parmesano',
-                                    'crotones',
-                                    'chile',
-                                    'almendras',
-                                    'aceitunas'
-                                ]
-                            }
-
-                        ]
-                    }
-
-                },
-            ]
-
-        },
+    orden:'',
+    menu:[
         {
             nombre: 'desayunos',
             comidas: [
                 {
                     nombre: 'omelet',
-                    precio: 100,
-                    descripcion: 'Tortilla de huevo rellena de ingredientes al gusto.',
-                    urlImage: 'static/omelet.jpg',
-                    typeInput: 'normal',
-                    input: {
-                        tipo: 'select',
-                        opciones: [
-                            'zanahoria',
-                            'tomate',
-                            'betabel',
-                            'champiñón',
-                            'jalapeño',
-                            'cebolla',
-                            'aceituna',
-                            'piña',
-                            'morrón',
-                            'panela',
-                            'pasta',
-                            'apio',
-                            'col morado',
-                            'pepino',
-                            'espinaca',
-                            'germinado',
-                            'elote',
-                            'brócoli',
-                            'jamón',
-                            'chorizo',
-                            'queso manchego'
-                        ]
-                    }
-
+                    precio: 100
                 },
                 {
                     nombre: 'chilaquiles',
-                    precio: 110,
-                    descripcion: 'Totopos dorados con pollo o huevo bañado en salsa de su elección.',
-                    urlImage: 'static/chilaquiles.jpg',
-                    typeInput: 'normal',
-                    input: {
-                        tipo: 'optional',
-                        opciones: [
-                            'Verde',
-                            'Rojo',
-                            'Mole'
-                        ]
-
-                    }
+                    precio: 110
                 },
                 {
                     nombre: 'hot cakes',
-                    precio: 120,
-                    descripcion: 'Mezcla de harina de hot cakes de la casa con plátano.',
-                    urlImage: 'static/hotcakes.jpg',
-                    typeInput: 'normal',
-                    input: {
-                        tipo: 'multiple'
-
-                    }
+                    precio: 120
                 }
             ]
 
@@ -174,24 +30,15 @@ const dataFixed = {
             comidas: [
                 {
                     nombre: 'guacamole',
-                    precio: 90,
-                    descripcion: 'Aguacate machacado acompañado con jitomate, limón, cebolla, cilantro, aceite de oliva y totopos.',
-                    urlImage: 'static/guacamole.jpg',
-                    typeInput: 'normal',
+                    precio: 90
                 },
                 {
                     nombre: 'tostada francesa',
-                    precio: 80,
-                    descripcion: 'Pan mojado con una mezcla de huevo y leche, frito acompañado con lechera o miel.',
-                    urlImage: 'static/tostadasfrancesas.jpg',
-                    typeInput: 'normal'
+                    precio: 80
                 },
                 {
                     nombre: 'orden de pan tostado',
-                    precio: 100,
-                    descripcion: 'Pan perfectamente tostado con mermelada de fresa y mantequilla para acompañar. ',
-                    urlImage: 'static/pantostado.jpg',
-                    typeInput: 'normal'
+                    precio: 100
                 }
             ]
 
