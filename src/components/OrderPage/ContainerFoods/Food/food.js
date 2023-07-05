281969179e36950cc5cf4b9c07d834220cdb70a5
sahid <sahidjimenez@wolfsellsers.com>
2023-07-05 10:24:47 -0600

    forms

--- src/components/OrderPage/ContainerFoods/Food/food.js
+++ src/components/OrderPage/ContainerFoods/Food/food.js
@@ -1,33 +1,59 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext, useState, useCallback } from 'react'
 import css from './food.module.css'
-
-const comida = {
-    nombre: 'omelet',
-    precio: 100,
-    descripcion: 'Tortilla de huevo rellena de ingredientes al gusto.',
-    url: 'static/omelet.jpg'
-}
-
-const { nombre, precio, descripcion, url } = comida;
-
+import { DataContext } from '../../../../context/DataContext';
+import Modal from '../Modal/modal';
+import { useOrderPage } from '../../Hooks/useOrderPage';
+import { Button } from '@mui/material';
 const Food = props => {
-    /*  console.log( props) */
-    const { nombre:name, comidas:food } = props.props ? props.props:'';
-   
-    console.log(food)
 
+    const {
+        handleClickOpen,
+        setSelectedFood,
+        selectedFood,
+        open,
+        handleClose,
+
+    } = props
+
+    const { nombre: name, comidas: food } = props.props ? props.props : '';
+
+    const containerFood = (
+        food.map((element, i) => {
+
+            return (
+
+                <div key={i} onClick={() => {
+                    setSelectedFood(element)
+                    handleClickOpen()
+                }} className='container flex py-2'>
+
+                    <div className={css.containerInfo}>
+                        <span className='font-bold uppercase'>{element.nombre}</span>
+                        <span className='text-xs'>$ {element.precio}</span>
+                        <span className='text-xs'>{element.descripcion}</span>
+                    </div>
+                    <div className={css.imagen}>
+                        <img alt='prueba' src={element.urlImage}></img>
+                    </div>
+
+                </div>
+
+            )
+        })
+    )
     return (
-        <div className='container flex py-2'>
-
+        <div >
             <div className={css.containerInfo}>
                 <span className='font-bold uppercase'>{name}</span>
-                <span className='text-xs'>$ {precio}</span>
-                <span className='text-xs'>{descripcion}</span>
             </div>
-            <div className={css.imagen}>
-                <img alt='prueba' src={url}></img>
-            </div>
+            {containerFood}
+            <Modal
+                valores={selectedFood}
+                handleClickOpen={handleClickOpen}
+                open={open}
+                handleClose={handleClose}
+            />
+           
         </div>
     )
 }
--- src/components/OrderPage/orderPage.js
+++ src/components/OrderPage/orderPage.js
@@ -1,19 +1,14 @@
-import React, { useContext } from 'react';
-import { DataContext } from '../../context/DataContext';
+import React from 'react';
 import Header from '../Header/header';
 import ContainerFoods from './ContainerFoods/containerFoods';
 import Navbar from './Navbar/navbar';
 import Resume from './Resume/resume';
 const OrderPage = () => {
 
-  const { data, useData } = useContext(DataContext)
-
 /*   console.log(data) */
 
-  const { nombre } = data;
   return (
     <>
-
       <Header />
       <div className='container p-3'>
         <p  className='text-center text-[24px]' > Fast'n Green Campeche</p>
@@ -21,9 +16,7 @@ const OrderPage = () => {
       <Resume />
       <Navbar />
       <ContainerFoods/>
-
     </>
-
   )
 }
 
