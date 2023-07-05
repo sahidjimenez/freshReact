2926417762f3e48e722a6eedb5e38761858d6349
sahid <sahidjimenez@wolfsellsers.com>
2023-07-05 11:04:28 -0600

    Revert "Revert "forms""
    This reverts commit 4050c50302eecff2d6767bd830c3434d7605ab93.

--- src/components/OrderPage/ContainerFoods/containerFoods.js
+++ src/components/OrderPage/ContainerFoods/containerFoods.js
@@ -1,25 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import Food from './Food/food';
 import css from './containerFoods.module.css';
 import { DataContext } from '../../../context/DataContext';
+import Modal from './Modal/modal';
+import { useEffect } from 'react';
 
 const ContainerFoods = () => {
 
-    const { data } = useContext(DataContext);
+    const { data, setData } = useContext(DataContext);
     const { menu } = data;
+    const [open, setOpen] = useState(false)
+    const [selectedFood, setSelectedFood] = useState({})
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const sahid = (item) => {
+        handleClickOpen()
+        setData({
+            ...data,
+            total: "10"
+        })
+    }
+
     return (
         <div className={css.container}>
             <div className={css.containerFoods}>
-                {menu.map((element) => {
+                {menu.map((element, i) => {
 
                     return (
-                        <>
-                            <Food props={element} />
-                        </>
+                        <div key={i}>
+
+                            <div >
+                                <Food
+                                    props={element}
+                                    handleClickOpen={handleClickOpen}
+                                    setSelectedFood={setSelectedFood}
+                                    selectedFood={selectedFood}
+                                    open={open}
+                                    handleClose={handleClose}
+                             
+
+                                />
+                            </div>
+
+                        </div>
                     )
-                    
                 })}
-                <Food />
+
             </div>
         </div>
 
